d5375d036a6e9a0d0c9652775967c26adec55cec
Erik Santos <erik.pereira@aluno.fmpsc.edu.br>
2024-09-26 16:22:23 -0300

    soma_multiplicacao_divisao.py

--- soma_multiplicacao_divisao.py
+++ soma_multiplicacao_divisao.py
@@ -1,10 +1,17 @@
-n1 = int(input('digite um valor:'))
-n2 = int(input('Digite outro Valor:'))
-s = n1 + n2
-m = n1 * n2
-d = n1 / n2
-di = n1 // n2
-e = n1 ** n2
-print(f'O resultado da soma é {s}, o resultado da multiplicação é {m}, da divisão {d:2f}',end=' ')
-print(f'e o resultado da divisão inteira é {di} e o resultado da potência é {e}')
+print('Protótipo de calculadora Python Nº2')
+# Solicitar valores ao usuário
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro Valor: '))
+#  Verificar se o segundo valor é zero para evitar divisão por zero
+if  n2 != 0:
+    s = n1 + n2
+    m = n1 * n2
+    d = n1 / n2
+    di = n1 // n2
+    e = n1 ** n2
+# Exibir resultados
+    print('O valor da soma é {}, o produto é {} e a divisão é {}'.format(s,m,d))
+    print('A divisão inteira é {} e a potência {}'.format(di,e))
+else:
+    print('Não é possível dividir por zero')
  
