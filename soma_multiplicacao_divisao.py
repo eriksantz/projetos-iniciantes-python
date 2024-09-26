2134f78c79aee5feb34a2dd648f1bf7d72327b6a
Erik Santos <erik.pereira@aluno.fmpsc.edu.br>
2024-09-26 16:19:38 -0300

    soma_multiplicacao_divisao.py

---
+++ soma_multiplicacao_divisao.py
@@ -0,0 +1,10 @@
+n1 = int(input('digite um valor:'))
+n2 = int(input('Digite outro Valor:'))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print(f'O resultado da soma é {s}, o resultado da multiplicação é {m}, da divisão {d:2f}',end=' ')
+print(f'e o resultado da divisão inteira é {di} e o resultado da potência é {e}')
+ 
